a93dcb91f8c4457f042505b88dd4cfe5ec6d3188
Jkevin <kevinjiang.366@gmail.com>
2025-12-16 00:58:16 +0800

    feat: 测试图床可用性

--- nuxt-blog/app/pages/camera/gallery.vue
+++ nuxt-blog/app/pages/camera/gallery.vue
@@ -1,28 +1,31 @@
-<template>
-  <div class="pl-10 pr-10 pt-5">
-    <div v-for="year in data">
-      <div class="text-3xl">{{ year.date }}</div>
-      <div v-for="image in year.images">
-        <img :src="image.src" />
-      </div>
-    </div>
-    <n-button type="tertiary">
-      Tertiary
-    </n-button>
-  </div>
-</template>
 <script setup>
+import { NImage } from 'naive-ui';
 
 const data = ref([
   {
     date: '20251215',
     images:[
       {
-        src:""
+        src:"https://cdn.jsdelivr.net/gh/JKevin-5/blog@feat/init-nuxt-blog/nuxt-blog/assets/imges/cat-251214.jpg"
       },{
-        src:""
+        src:"https://cdn.jsdelivr.net/gh/JKevin-5/blog@feat/init-nuxt-blog/nuxt-blog/assets/imges/cat-251214.jpg"
       }
     ]
   }
 ])
 </script>
+<template>
+  <div class="pl-10 pr-10 pt-5">
+    <div v-for="year in data">
+      <div class="text-3xl">{{ year.date }}</div>
+      <div class="flex gap-2">
+        <div v-for="image in year.images">
+          <n-image
+            width="300"
+            :src="image.src"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
